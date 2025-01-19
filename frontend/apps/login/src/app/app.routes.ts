d40859fb19ba18ed84f51d48356d6e4043eb3980
rodionmaulenov <natalia.maulenova@gmail.com>
2025-01-20 00:43:56 +0200

    change the env url

--- frontend/apps/login/src/app/app.routes.ts
+++ frontend/apps/login/src/app/app.routes.ts
@@ -3,5 +3,5 @@ import {LoginPageComponent} from './auth/login-page/login-page.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
-  {path: '**', redirectTo: '/login'}
+  {path: '**', redirectTo: 'login'}
 ];
