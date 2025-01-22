e37e9305665bee2e8d436b68dad5da59821f0531
rodionmaulenov <natalia.maulenova@gmail.com>
2025-01-22 17:11:58 +0200

    v

--- frontend/unified-app/src/app/app.routes.ts
+++ frontend/unified-app/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import {Routes} from '@angular/router';
 
 
 export const routes: Routes = [
-  {path: 'login', loadChildren: () => import('./login/login.routes').then(m => m.routes)},
+  {path: '', loadChildren: () => import('./login/login.routes').then(m => m.routes)},
   {path: 'duration', loadChildren: () => import('./duration/duration.routes').then(m => m.routes)},
 ]
