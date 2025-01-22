58e55b2895265c85d720e20de43e0949d0c36aea
rodionmaulenov <natalia.maulenova@gmail.com>
2025-01-22 16:18:12 +0200

    gloabl change with lazy loading

--- frontend/apps/duration/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://biotexcomapp.docktor-di.com/api/',
-};
--- frontend/apps/login/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import {Routes} from '@angular/router';
-import {LoginPageComponent} from './auth/login-page/login-page.component';
-
-export const routes: Routes = [
-  {path: 'login', component: LoginPageComponent},
-  {path: '**', redirectTo: '/login'}
-];
